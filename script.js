ee762f228c11032ba37a79a001530af53b3185f5
Adryan Carvalho <adryansc.18@gmail.com>
2026-02-26 11:22:59 -0300

    feat:POST cadastro

---
+++ script.js
@@ -0,0 +1,20 @@
+const api = axios.create({
+    baseURL: "http://localhost:3000"
+})
+
+document.getElementById("RegistrarForm").addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    const name = document.getElementById("Nome").value
+    const email = document.getElementById("Email").value
+    const password = document.getElementById("Senha").value
+    const corpo = {name, email, password}
+    try{
+        const response = await api.post("/users", corpo)
+        console.log("Usuario Cadastrado: ", response.data)
+        alert("Usuario cadastrado com sucesso!")
+    } catch(error){
+        console.log("Erro:", error)
+        alert("Erro ao cadastrar usuario!")
+    }
+})
